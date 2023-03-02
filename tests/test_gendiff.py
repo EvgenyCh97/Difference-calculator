fe2edd301da1a547d13be306640417a08e7fa780
Евгений Чайка <chay67ka26@gmail.com>
2023-03-02 17:03:31 +0800

    delete "123" str from json files and from tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -17,13 +17,10 @@ def json_dict2():
 
 
 def test_get_dicts_from_():
-    result = gendiff.get_dicts_from_(JSON1,
-                                     JSON2)
+    result = gendiff.get_dicts_from_(JSON1, JSON2)
     assert result == ({'host': 'hexlet.io', 'timeout': 50,
-                       'proxy': '123.234.53.22', 'follow': False, '123': 1},
-                      {'timeout': 20, 'verbose': True, 'host': 'hexlet.io',
-                       '123': 2}
-                      )
+                       'proxy': '123.234.53.22', 'follow': False},
+                      {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
     result = gendiff.get_dicts_from_('tests/fixtures/file1.yaml',
                                      'tests/fixtures/file2.yml')
     assert result == ({'a': 1, 'b': None, 'c': 3, 'd': 4},
@@ -34,19 +31,15 @@ def test_get_items_list(json_dict1, json_dict2):
     result1 = gendiff.get_items_list(json_dict1)
     result2 = gendiff.get_items_list(json_dict2)
     assert result1 == [('host', 'hexlet.io'), ('timeout', 50),
-                       ('proxy', '123.234.53.22'), ('follow', False),
-                       ('123', 1)
-                       ]
+                       ('proxy', '123.234.53.22'), ('follow', False)]
     assert result2 == [('timeout', 20), ('verbose', True),
-                       ('host', 'hexlet.io'), ('123', 2)
-                       ]
+                       ('host', 'hexlet.io')]
 
 
 def test_get_keys_set_from_(json_dict1, json_dict2):
     result = gendiff.get_keys_set_from_(json_dict1, json_dict2)
-    assert result == ({'proxy', '123', 'timeout', 'follow', 'host'},
-                      {'timeout', '123', 'verbose', 'host'}
-                      )
+    assert result == ({'proxy', 'timeout', 'follow', 'host'},
+                      {'timeout', 'verbose', 'host'})
 
 
 def test_encode_(json_dict1, json_dict2):
@@ -80,8 +73,8 @@ def test_get_old_lines():
 
 def test_get_updated_lines():
     result = gendiff.get_updated_lines(JSON1, JSON2)
-    assert set(result[0]) == {('- timeout', 50), ('- 123', 1)}
-    assert set(result[1]) == {('+ timeout', 20), ('+ 123', 2)}
+    assert set(result[0]) == {('- timeout', 50)}
+    assert set(result[1]) == {('+ timeout', 20)}
 
 
 def test_get_result_list():
@@ -89,12 +82,10 @@ def test_get_result_list():
                                      gendiff.get_matches,
                                      gendiff.get_new_lines,
                                      gendiff.get_old_lines,
-                                     gendiff.get_updated_lines
-                                     )
+                                     gendiff.get_updated_lines)
     assert set(result) == {('  host', 'hexlet.io'), ('+ verbose', True),
                            ('- proxy', '123.234.53.22'), ('- follow', False),
-                           ('- timeout', 50), ('- 123', 1), ('+ timeout', 20),
-                           ('+ 123', 2)}
+                           ('- timeout', 50), ('+ timeout', 20)}
 
 
 def test_generate_diff():
