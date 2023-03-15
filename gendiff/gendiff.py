33e33890f452998a5c7b0d97ef572fc9991ef604
Евгений Чайка <chay67ka26@gmail.com>
2023-03-15 22:45:56 +0800

    refactoring gendiff.py and update test_gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,21 +3,11 @@ import yaml
 from yaml import CLoader as Loader
 
 
-def get_dicts_from_(file_path1, file_path2):
-    if file_path1.endswith('.yml') or file_path1.endswith('.yaml'):
-        return (yaml.load(open(file_path1), Loader=Loader),
-                yaml.load(open(file_path2), Loader=Loader))
-    if file_path1.endswith('.json'):
-        return json.load(open(file_path1)), json.load(open(file_path2))
-
-
-def get_items_list(dictionary):
-    return list(dictionary.items())
-
-
-def get_keys_set_from_(dict1, dict2):
-    return ({items[0] for items in get_items_list(dict1)},
-            {items[0] for items in get_items_list(dict2)})
+def get_dict_from_(file_path):
+    if file_path.endswith('.yml') or file_path.endswith('.yaml'):
+        return yaml.load(open(file_path), Loader=Loader)
+    if file_path.endswith('.json'):
+        return json.load(open(file_path))
 
 
 def encode_(encoded_list):
@@ -28,62 +18,25 @@ def sort_(sortable_list):
     return sorted(sortable_list, key=lambda items: items[0][2])
 
 
-def get_matches(file_path1, file_path2):
-    dict1, dict2 = get_dicts_from_(file_path1, file_path2)
-    set1 = set(get_items_list(dict1))
-    set2 = set(get_items_list(dict2))
-    set1.intersection_update(set2)
-    return [(f'  {key}', dict1.get(key))
-            for key in [items[0]
-                        for items in set1]]
-
-
-def get_new_lines(file_path1, file_path2):
-    dict1, dict2 = get_dicts_from_(file_path1, file_path2)
-    set1, set2 = get_keys_set_from_(dict1, dict2)
-    set2.difference_update(set1)
-    return [(f'+ {key}', dict2.get(key)) for key in set2]
-
-
-def get_old_lines(file_path1, file_path2):
-    dict1, dict2 = get_dicts_from_(file_path1, file_path2)
-    set1, set2 = get_keys_set_from_(dict1, dict2)
-    set1.difference_update(set2)
-    return [(f'- {key}', dict1.get(key)) for key in set1]
-
-
-def get_updated_lines(file_path1, file_path2):
-    dict1, dict2 = get_dicts_from_(file_path1, file_path2)
-    set1, set2 = get_keys_set_from_(dict1, dict2)
-    set1.intersection_update(set2)
-    diff1, diff2 = dict(), dict()
-    for key in set1:
-        if dict1[key] != dict2[key]:
-            diff1[f'- {key}'] = dict1.get(key)
-            diff2[f'+ {key}'] = dict2.get(key)
-    result = list()
-    result.append(get_items_list(diff1))
-    result.append(get_items_list(diff2))
-    return result
-
-
-def get_result_list(file_path1, file_path2, *args):
-    result = list()
-    for element in [func(file_path1, file_path2) for func in args]:
-        [result.append(items_tuple)
-         for items_tuple in element if type(element[0]) == tuple]
-        [[result.append(item)
-          for item in items_list]
-         for items_list in element if type(element[0]) == list]
+def get_diff(file_path1, file_path2):
+    dict1 = get_dict_from_(file_path1)
+    dict2 = get_dict_from_(file_path2)
+    result = []
+    for key in dict1.keys():
+        if dict2.get(key):
+            if dict1[key] == dict2[key]:
+                result.append((f'  {key}', dict1.get(key)))
+            else:
+                result.append((f'- {key}', dict1.get(key)))
+                result.append((f'+ {key}', dict2.get(key)))
+        else:
+            result.append((f'- {key}', dict1.get(key)))
+    for key in dict2.keys():
+        if dict1.get(key) is None:
+            result.append((f'+ {key}', dict2.get(key)))
     return result
 
 
 def generate_diff(file_path1, file_path2):
-    return encode_(sort_(get_result_list(file_path1, file_path2,
-                                         get_matches,
-                                         get_new_lines,
-                                         get_old_lines,
-                                         get_updated_lines
-                                         )
-                         )
-                   ).replace('"', '').replace(',', '')
+    return encode_(sort_(get_diff(
+        file_path1, file_path2))).replace('"', '').replace(',', '')
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,6 +4,8 @@ from gendiff import gendiff
 
 JSON1 = 'tests/fixtures/file1.json'
 JSON2 = 'tests/fixtures/file2.json'
+YAML1 = 'tests/fixtures/file1.yaml'
+YAML2 = 'tests/fixtures/file2.yml'
 
 
 @pytest.fixture
@@ -16,39 +18,30 @@ def json_dict2():
     return json.load(open(JSON2))
 
 
-def test_get_dicts_from_():
-    result = gendiff.get_dicts_from_(JSON1, JSON2)
+def test_get_dict_from_():
+    result = gendiff.get_dict_from_(JSON1)
     assert result == ({'host': 'hexlet.io', 'timeout': 50,
-                       'proxy': '123.234.53.22', 'follow': False},
-                      {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
-    result = gendiff.get_dicts_from_('tests/fixtures/file1.yaml',
-                                     'tests/fixtures/file2.yml')
-    assert result == ({'a': 1, 'b': None, 'c': 3, 'd': 4},
-                      {'a': 1, 'b': None, 'c': 5, 'd': 7})
+                       'proxy': '123.234.53.22', 'follow': False})
 
+    result = gendiff.get_dict_from_(JSON2)
+    assert result == ({'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
 
-def test_get_items_list(json_dict1, json_dict2):
-    result1 = gendiff.get_items_list(json_dict1)
-    result2 = gendiff.get_items_list(json_dict2)
-    assert result1 == [('host', 'hexlet.io'), ('timeout', 50),
-                       ('proxy', '123.234.53.22'), ('follow', False)]
-    assert result2 == [('timeout', 20), ('verbose', True),
-                       ('host', 'hexlet.io')]
+    result = gendiff.get_dict_from_(YAML1)
+    assert result == ({'host': 'hexlet.io', 'timeout': 50,
+                       'proxy': '123.234.53.22', 'follow': False})
 
-
-def test_get_keys_set_from_(json_dict1, json_dict2):
-    result = gendiff.get_keys_set_from_(json_dict1, json_dict2)
-    assert result == ({'proxy', 'timeout', 'follow', 'host'},
-                      {'timeout', 'verbose', 'host'})
+    result = gendiff.get_dict_from_(YAML2)
+    assert result == ({'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
 
 
 def test_encode_(json_dict1, json_dict2):
-    result1 = gendiff.encode_(json_dict1)
-    result2 = gendiff.encode_(json_dict2)
+    result = gendiff.encode_(json_dict1)
     with open(JSON1, 'r') as json_file1:
-        assert result1 == json_file1.read()
+        assert result == json_file1.read()
+
+    result = gendiff.encode_(json_dict2)
     with open(JSON2, 'r') as json_file2:
-        assert result2 == json_file2.read()
+        assert result == json_file2.read()
 
 
 def test_sort_():
@@ -56,39 +49,11 @@ def test_sort_():
     assert result == [('  a', 'first'), ('  b', 'second')]
 
 
-def test_get_matches():
-    result = gendiff.get_matches(JSON1, JSON2)
-    assert set(result) == {('  host', 'hexlet.io')}
-
-
-def test_get_new_lines():
-    result = gendiff.get_new_lines(JSON1, JSON2)
-    assert set(result) == {('+ verbose', True)}
-
-
-def test_get_old_lines():
-    result = gendiff.get_old_lines(JSON1, JSON2)
-    assert set(result) == {('- proxy', '123.234.53.22'), ('- follow', False)}
-
-
-def test_get_updated_lines():
-    result = gendiff.get_updated_lines(JSON1, JSON2)
-    assert set(result[0]) == {('- timeout', 50)}
-    assert set(result[1]) == {('+ timeout', 20)}
-
-
-def test_get_result_list():
-    result = gendiff.get_result_list(JSON1, JSON2,
-                                     gendiff.get_matches,
-                                     gendiff.get_new_lines,
-                                     gendiff.get_old_lines,
-                                     gendiff.get_updated_lines)
-    assert set(result) == {('  host', 'hexlet.io'), ('+ verbose', True),
-                           ('- proxy', '123.234.53.22'), ('- follow', False),
-                           ('- timeout', 50), ('+ timeout', 20)}
-
-
 def test_generate_diff():
     result = gendiff.generate_diff(JSON1, JSON2)
+    with open('tests/fixtures/diff', 'r') as diff:
+        assert result == diff.read()
+
+    result = gendiff.generate_diff(YAML1, YAML2)
     with open('tests/fixtures/diff', 'r') as diff:
         assert result == diff.read()
