63bc5cecc6da802bd3f1cabb98587bf0a5b73430
Евгений Чайка <chay67ka26@gmail.com>
2023-04-14 20:52:08 +0800

    delete encode_ and sort_ functions, refactor get_result_list func and rename this to compare_files, refactor generate_diff func, fix tests

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -11,34 +11,85 @@ def get_dict_from_(file_path):
         return json.load(open(file_path))
 
 
-def encode_(encoded_list):
-    return json.dumps(dict(encoded_list), indent=2)
-
-
-def sort_(sortable_dict):
-    sorted_keys = sorted(sortable_dict, key=lambda items: items[2])
-    return [(key, sortable_dict[key]) for key in sorted_keys]
-
-
-def get_result_list(file_path1, file_path2):
-    dict1 = get_dict_from_(file_path1)
-    dict2 = get_dict_from_(file_path2)
-    result = dict()
-    for key in dict1.keys():
-        if dict2.get(key):
-            if dict1[key] == dict2[key]:
-                result[f'  {key}'] = dict1.get(key)
-            else:
-                result[f'- {key}'] = dict1.get(key)
-                result[f'+ {key}'] = dict2.get(key)
+def convert_bool_and_null_to_str(tree):
+    for elem in tree:
+        if type(tree[elem]) != dict:
+            if tree[elem] is None:
+                tree[elem] = 'null'
+            elif tree[elem] is True:
+                tree[elem] = 'true'
+            elif tree[elem] is False:
+                tree[elem] = 'false'
         else:
-            result[f'- {key}'] = dict1.get(key)
-    for key in dict2.keys():
-        if dict1.get(key) is None:
-            result[f'+ {key}'] = dict2[key]
-    return result
+            children = tree[elem]
+            convert_bool_and_null_to_str(children)
+    return tree
+
+
+def compare_files():
+    def inner(dict1, dict2, depth=1):
+        result = dict()
+        for key in dict1.keys():
+            if dict2.get(key) and type(dict1[key]) == dict and type(
+                    dict2[key]) == dict:
+                result[key] = {'type': 'nested',
+                               'value': inner(dict1[key], dict2[key],
+                                              depth + 1), 'depth': depth}
+            elif dict2.get(key):
+                if dict1[key] == dict2[key]:
+                    result[key] = {'type': 'unchanged', 'value': dict1.get(key),
+                                   'depth': depth}
+                else:
+                    if type(dict1[key]) == dict:
+                        result[key] = {'type': 'changed',
+                                       'value': dict2.get(key),
+                                       'old_value': inner(dict1[key],
+                                                          dict1[key],
+                                                          depth + 1),
+                                       'depth': depth}
+                    elif type(dict2[key]) == dict:
+                        result[key] = {'type': 'changed',
+                                       'value': inner(dict2[key], dict2[key],
+                                                      depth + 1),
+                                       'old_value': dict1.get(key),
+                                       'depth': depth}
+                    else:
+                        result[key] = {'type': 'changed',
+                                       'value': dict2.get(key),
+                                       'old_value': dict1.get(key),
+                                       'depth': depth}
+            else:
+                if type(dict1[key]) == dict:
+                    result[key] = {'type': 'deleted', 'value': inner(dict1[key],
+                                                                     dict1[key],
+                                                                     depth + 1),
+                                   'depth': depth}
+                else:
+                    result[key] = {'type': 'deleted', 'value': dict1.get(key),
+                                   'depth': depth}
+        for key in dict2.keys():
+            if dict1.get(key) is None:
+                if type(dict2[key]) == dict:
+                    result[key] = {'type': 'added',
+                                   'value': inner(dict2.get(key),
+                                                  dict2.get(key), depth + 1),
+                                   'depth': depth}
+                else:
+                    result[key] = {'type': 'added', 'value': dict2.get(key),
+                                   'depth': depth}
+        return result
+    return inner
 
 
 def generate_diff(file_path1, file_path2):
-    return encode_(sort_(get_result_list(
-        file_path1, file_path2))).replace('"', '').replace(',', '')
+    dict1 = convert_bool_and_null_to_str(get_dict_from_(file_path1))
+    dict2 = convert_bool_and_null_to_str(get_dict_from_(file_path2))
+    compare = compare_files()
+    diff = '{\n'
+    result_list = stylish.get_stylish(compare(dict1, dict2))
+    for string in result_list:
+        if string == result_list[-1]:
+            diff += string
+        else:
+            diff += string + '\n'
+    return diff.replace('"', '')
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,75 +1,108 @@
 import pytest
 import json
-from gendiff import gendiff
+from gendiff import stylish, gendiff
 
-JSON1 = 'tests/fixtures/file1.json'
-JSON2 = 'tests/fixtures/file2.json'
+
+JSON1 = 'tests/fixtures/file3.json'
+JSON2 = 'tests/fixtures/file4.json'
 YAML1 = 'tests/fixtures/file1.yaml'
 YAML2 = 'tests/fixtures/file2.yml'
 
 
-@pytest.fixture
-def json_dict1():
-    return json.load(open(JSON1))
-
-
-@pytest.fixture
-def json_dict2():
-    return json.load(open(JSON2))
-
-
 def test_get_dict_from_():
     result = gendiff.get_dict_from_(JSON1)
-    assert result == ({'host': 'hexlet.io', 'timeout': 50,
-                       'proxy': '123.234.53.22', 'follow': False})
+    assert result == {'common': {
+        'setting1': 'Value 1',
+        'setting2': 200,
+        'setting3': True,
+        'setting6':
+            {'key': 'value', 'doge': {'wow': ''}}},
+        'group1': {'baz': 'bas', 'foo': 'bar',
+                   'nest': {'key': 'value'}},
+        'group2': {'abc': 12345, 'deep': {'id': 45}}}
 
     result = gendiff.get_dict_from_(JSON2)
-    assert result == ({'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
+    assert result == {'common': {
+        'follow': False,
+        'setting1': 'Value 1',
+        'setting3': None,
+        'setting4': 'blah blah',
+        'setting5': {'key5': 'value5'},
+        'setting6': {
+            'key': 'value', 'ops': 'vops', 'doge': {'wow': 'so much'}}},
+        'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'},
+        'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
 
     result = gendiff.get_dict_from_(YAML1)
-    assert result == ({'host': 'hexlet.io', 'timeout': 50,
-                       'proxy': '123.234.53.22', 'follow': False})
+    assert result == {'host': 'hexlet.io', 'timeout': 50,
+                      'proxy': '123.234.53.22', 'follow': False}
 
     result = gendiff.get_dict_from_(YAML2)
-    assert result == ({'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})
+    assert result == {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
 
 
-def test_encode_(json_dict1, json_dict2):
-    result = gendiff.encode_(json_dict1)
-    with open(JSON1, 'r') as json_file1:
-        assert result == json_file1.read()
-
-    result = gendiff.encode_(json_dict2)
-    with open(JSON2, 'r') as json_file2:
-        assert result == json_file2.read()
-
-
-def test_sort_():
-    result = gendiff.sort_({'  host': 'hexlet.io', '- timeout': 50,
-                            '+ timeout': 20, '- proxy': '123.234.53.22',
-                            '- follow': False, '+ verbose': True})
-    assert result == [('- follow', False), ('  host', 'hexlet.io'),
-                      ('- proxy', '123.234.53.22'), ('- timeout', 50),
-                      ('+ timeout', 20), ('+ verbose', True)]
+def test_convert_bool_and_null_to_str():
+    result = gendiff.convert_bool_and_null_to_str({'common': {
+        'setting1': False,
+        'setting2': None,
+        'setting3': True,
+        'setting6': {
+            'key': 'value',
+            'doge': {
+                'wow': ''}}}})
+    assert result == {'common': {
+        'setting1': 'false', 'setting2': 'null', 'setting3': 'true',
+        'setting6': {'key': 'value', 'doge': {'wow': ''}}}}
 
 
-def test_get_result_list():
-    result = gendiff.get_result_list(JSON1, JSON2)
-    assert result == {'  host': 'hexlet.io', '- timeout': 50, '+ timeout': 20,
-                      '- proxy': '123.234.53.22', '- follow': False,
-                      '+ verbose': True}
+def test_get_stylish():
+    result = stylish.get_stylish({
+        'group1': {
+            'type': 'added', 'value': {
+                'foo': {'type': 'unchanged', 'value': 'bar', 'depth': 2},
+                'baz': {'type': 'unchanged', 'value': 'bars', 'depth': 2},
+                'nest': {'type': 'unchanged', 'value': 'str', 'depth': 2}},
+            'depth': 1}})
+    assert result == ['  + group1: {', '        baz: bars',
+                      '        foo: bar', '        nest: str', '    }', '}']
 
-    result = gendiff.get_result_list(YAML1, YAML2)
-    assert result == {'  host': 'hexlet.io', '- timeout': 50, '+ timeout': 20,
-                      '- proxy': '123.234.53.22', '- follow': False,
-                      '+ verbose': True}
+
+def test_compare_files():
+    compare = gendiff.compare_files()
+    result = compare(
+        {'group1': {'baz': 'bas', 'foo': 'bar', 'nest': {'key': 'value'}}},
+        {'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'}})
+    assert result == {'group1': {
+        'type': 'nested', 'value': {
+            'baz': {'type': 'changed',
+                    'value': 'bars', 'old_value': 'bas', 'depth': 2},
+            'foo': {'type': 'unchanged', 'value': 'bar', 'depth': 2},
+            'nest': {'type': 'changed',
+                     'value': 'str',
+                     'old_value': {
+                         'key': {'type': 'unchanged',
+                                 'value': 'value', 'depth': 3}},
+                     'depth': 2}},
+        'depth': 1}}
+
+    result = compare(
+        {'host': 'hexlet.io', 'timeout': 50,
+         'proxy': '123.234.53.22', 'follow': 'false'},
+        {'timeout': 20, 'verbose': 'true', 'host': 'hexlet.io'})
+    assert result == {
+        'host': {'type': 'unchanged', 'value': 'hexlet.io', 'depth': 1},
+        'timeout': {'type': 'changed', 'value': 20, 'old_value': 50,
+                    'depth': 1},
+        'proxy': {'type': 'deleted', 'value': '123.234.53.22', 'depth': 1},
+        'follow': {'type': 'deleted', 'value': 'false', 'depth': 1},
+        'verbose': {'type': 'added', 'value': 'true', 'depth': 1}}
 
 
 def test_generate_diff():
     result = gendiff.generate_diff(JSON1, JSON2)
-    with open('tests/fixtures/diff', 'r') as diff:
-        assert result == diff.read()
+    with open('tests/fixtures/json_diff', 'r') as json_diff:
+        assert result == json_diff.read()
 
     result = gendiff.generate_diff(YAML1, YAML2)
-    with open('tests/fixtures/diff', 'r') as diff:
-        assert result == diff.read()
+    with open('tests/fixtures/yaml_diff', 'r') as yaml_diff:
+        assert result == yaml_diff.read()
