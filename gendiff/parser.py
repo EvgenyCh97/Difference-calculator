e7e95974b2fad3872e7b368444d0904af70cdf17
Евгений Чайка <chay67ka26@gmail.com>
2023-04-14 20:47:31 +0800

    edit names of parser arguments

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,8 +3,9 @@ import argparse
 parser = argparse.ArgumentParser(
     description='Compares two configuration files and shows a difference.'
 )
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-parser.add_argument('-f', '--format', help='set format of output')
+parser.add_argument('filepath_1', type=str, help='path to first file')
+parser.add_argument('filepath_2', type=str, help='path to second file')
+parser.add_argument('-f', '--format [type]', metavar='',
+                    help='set format of output (default: "stylish")')
 
 ARGS = parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.gendiff import generate_diff
 
 
 def main():
-    print(generate_diff(ARGS.first_file, ARGS.second_file))
+    print(generate_diff(ARGS.filepath_1, ARGS.filepath_2))
 
 
 if __name__ == '__main__':
