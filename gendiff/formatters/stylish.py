b3164d3215e140f644a38b15e5f292a63929e29d
Евгений Чайка <chay67ka26@gmail.com>
2023-05-15 19:37:47 +0800

    remove magic numbers in stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,5 +1,8 @@
 import json
 
+SPACES_PER_LVL = 4
+LEFT_SHIFT = 2
+
 
 def get_stylish(diff_dict):
     result = '{\n'
@@ -22,37 +25,54 @@ def complete_diff_list(diff_dict, diff_list):
         key_type = diff_dict[key]['type']
 
         if key_type == 'nested':
-            diff_list.append(f'{" " * (4 * depth - 2)}  {key}: ' + '{')
+            diff_list.append(f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}  '
+                             f'{key}: ' + '{')
             complete_diff_list(value, diff_list)
         if key_type == 'changed':
             old_value = converter(diff_dict[key]['old_value'])
             if type(old_value) == dict:
-                diff_list.append(f'{" " * (4 * depth - 2)}- {key}: ' + '{')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}- '
+                    f'{key}: ' + '{')
                 complete_diff_list(old_value, diff_list)
             else:
                 diff_list.append(
-                    f'{" " * (4 * depth - 2)}- {key}: {old_value}')
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}- '
+                    f'{key}: {old_value}')
             if type(value) == dict:
-                diff_list.append(f'{" " * (4 * depth - 2)}+ {key}: ' + '{')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}+ '
+                    f'{key}: ' + '{')
                 complete_diff_list(value, diff_list)
             else:
-                diff_list.append(f'{" " * (4 * depth - 2)}+ {key}: {value}')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}+ '
+                    f'{key}: {value}')
         if key_type == 'unchanged':
-            diff_list.append(f'{" " * (4 * depth - 2)}  {key}: {value}')
+            diff_list.append(f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}  '
+                             f'{key}: {value}')
         if key_type == 'deleted':
             if type(value) == dict:
-                diff_list.append(f'{" " * (4 * depth - 2)}- {key}: ' + '{')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}- '
+                    f'{key}: ' + '{')
                 complete_diff_list(value, diff_list)
             else:
-                diff_list.append(f'{" " * (4 * depth - 2)}- {key}: {value}')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}- '
+                    f'{key}: {value}')
         if key_type == 'added':
             if type(value) == dict:
-                diff_list.append(f'{" " * (4 * depth - 2)}+ {key}: ' + '{')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}+ '
+                    f'{key}: ' + '{')
                 complete_diff_list(value, diff_list)
             else:
-                diff_list.append(f'{" " * (4 * depth - 2)}+ {key}: {value}')
+                diff_list.append(
+                    f'{" " * (SPACES_PER_LVL * depth - LEFT_SHIFT)}+ '
+                    f'{key}: {value}')
         if key == sorted_keys[-1]:
-            diff_list.append(f'{" " * 4 * (depth - 1)}' + '}')
+            diff_list.append(f'{" " * SPACES_PER_LVL * (depth - 1)}' + '}')
 
 
 def converter(value):
