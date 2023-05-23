dfb5f47a0a6b48c636ccf7bc3b70318dd8fd0d7b
Евгений Чайка <chay67ka26@gmail.com>
2023-05-23 13:19:16 +0800

    rename converter to convert_to_plain func in plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -10,7 +10,7 @@ def complete_diff_list(diff_dict, diff_list: list, path=[], depth_lvl=1):
     sorted_keys = sorted(diff_dict)
     for key in sorted_keys:
 
-        value = converter(diff_dict[key]['value'])
+        value = convert_to_plain(diff_dict[key]['value'])
         key_type = diff_dict[key]['type']
 
         if depth_lvl == 1:
@@ -20,7 +20,7 @@ def complete_diff_list(diff_dict, diff_list: list, path=[], depth_lvl=1):
             children = diff_dict[key]['value']
             complete_diff_list(children, diff_list, path, depth_lvl + 1)
         if key_type == 'changed':
-            old_value = converter(diff_dict[key]['old_value'])
+            old_value = convert_to_plain(diff_dict[key]['old_value'])
             diff_list.append(f'Property \'{"".join(path)}{key}\' '
                              f'was updated. From {old_value} to {value}')
         if key_type == 'deleted':
@@ -33,7 +33,7 @@ def complete_diff_list(diff_dict, diff_list: list, path=[], depth_lvl=1):
     return diff_list
 
 
-def converter(value):
+def convert_to_plain(value):
     if type(value) == dict:
         return '[complex value]'
     elif type(value) not in [int, float]:
