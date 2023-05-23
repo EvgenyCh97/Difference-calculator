e5f3a0d9e471b6b8154bea76372c1fa2f7abbcd3
Евгений Чайка <chay67ka26@gmail.com>
2023-05-23 12:51:31 +0800

    move get_diff_dict(dict1, dict2) to diff variable in generate_diff func

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -46,9 +46,10 @@ def get_diff_dict(dict1, dict2):
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     dict1 = get_dict_from(file_path1)
     dict2 = get_dict_from(file_path2)
+    diff = get_diff_dict(dict1, dict2)
     if format_name == 'stylish':
-        return stylish.get_stylish(get_diff_dict(dict1, dict2))
+        return stylish.get_stylish(diff)
     if format_name == 'plain':
-        return plain.get_plain(get_diff_dict(dict1, dict2))
+        return plain.get_plain(diff)
     if format_name == 'json':
-        return get_json(get_diff_dict(dict1, dict2))
+        return get_json(diff)
