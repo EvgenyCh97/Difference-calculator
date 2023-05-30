1a5edf5961c640ce895f15813dce5839a20be0ee
Евгений Чайка <chay67ka26@gmail.com>
2023-05-30 13:10:14 +0800

    upgrade complete_plain_list func according to key changes 'value' -> 'new_value'

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -9,25 +9,26 @@ def get_plain(diff_dict):
 def complete_plain_list(diff_dict, diff_list: list, path=[], depth_lvl=1):
     sorted_keys = sorted(diff_dict)
     for key in sorted_keys:
-
-        value = convert_to_plain(diff_dict[key]['value'])
         key_type = diff_dict[key]['type']
-
         if depth_lvl == 1:
             path = []
-        if key_type == 'nested':
-            path.append(f'{key}.')
-            children = diff_dict[key]['value']
-            complete_plain_list(children, diff_list, path, depth_lvl + 1)
         if key_type == 'changed':
             old_value = convert_to_plain(diff_dict[key]['old_value'])
+            new_value = convert_to_plain(diff_dict[key]['new_value'])
             diff_list.append(f'Property \'{"".join(path)}{key}\' '
-                             f'was updated. From {old_value} to {value}')
-        if key_type == 'deleted':
-            diff_list.append(f'Property \'{"".join(path)}{key}\' was removed')
-        if key_type == 'added':
-            diff_list.append(f'Property \'{"".join(path)}{key}\' '
-                             f'was added with value: {value}')
+                             f'was updated. From {old_value} to {new_value}')
+        else:
+            value = convert_to_plain(diff_dict[key]['value'])
+            if key_type == 'nested':
+                path.append(f'{key}.')
+                children = diff_dict[key]['value']
+                complete_plain_list(children, diff_list, path, depth_lvl + 1)
+            if key_type == 'deleted':
+                diff_list.append(f'Property \'{"".join(path)}{key}\' was '
+                                 f'removed')
+            if key_type == 'added':
+                diff_list.append(f'Property \'{"".join(path)}{key}\' '
+                                 f'was added with value: {value}')
         if key == sorted_keys[-1] and path:
             path.pop()
     return diff_list
