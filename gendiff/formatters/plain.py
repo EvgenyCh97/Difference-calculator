740f9b9b21719a54b54cc59bfc6185caa3966ec2
Евгений Чайка <chay67ka26@gmail.com>
2023-06-05 12:49:56 +0800

    refactor complete_plain_list func: move diff_list parameter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -2,35 +2,42 @@ import json
 
 
 def get_plain(diff_dict):
-    diff_list = complete_plain_list(diff_dict, list())
+    diff_list = complete_plain_list(diff_dict)
     return '\n'.join(diff_list)
 
 
-def complete_plain_list(diff_dict, diff_list: list, path=[], depth_lvl=1):
-    sorted_keys = sorted(diff_dict)
-    for key in sorted_keys:
-        key_type = diff_dict[key]['type']
-        if depth_lvl == 1:
-            path = []
-        if key_type == 'changed':
-            old_value = convert_to_plain(diff_dict[key]['old_value'])
-            new_value = convert_to_plain(diff_dict[key]['new_value'])
-            diff_list.append(f'Property \'{"".join(path)}{key}\' '
-                             f'was updated. From {old_value} to {new_value}')
-        else:
-            value = convert_to_plain(diff_dict[key]['value'])
-            if key_type == 'nested':
-                path.append(f'{key}.')
-                children = diff_dict[key]['value']
-                complete_plain_list(children, diff_list, path, depth_lvl + 1)
-            if key_type == 'deleted':
-                diff_list.append(f'Property \'{"".join(path)}{key}\' was '
-                                 f'removed')
-            if key_type == 'added':
+def complete_plain_list(diff_dict):
+    diff_list = list()
+    path = list()
+
+    def inner(node, depth_lvl=1):
+        nonlocal path
+        sorted_keys = sorted(node)
+        for key in sorted_keys:
+            key_type = node[key]['type']
+            if depth_lvl == 1:
+                path = []
+            if key_type == 'changed':
+                old_value = convert_to_plain(node[key]['old_value'])
+                new_value = convert_to_plain(node[key]['new_value'])
                 diff_list.append(f'Property \'{"".join(path)}{key}\' '
-                                 f'was added with value: {value}')
-        if key == sorted_keys[-1] and path:
-            path.pop()
+                                 f'was updated. From {old_value} '
+                                 f'to {new_value}')
+            else:
+                value = convert_to_plain(node[key]['value'])
+                if key_type == 'nested':
+                    path.append(f'{key}.')
+                    children = node[key]['value']
+                    inner(children, depth_lvl + 1)
+                if key_type == 'deleted':
+                    diff_list.append(f'Property \'{"".join(path)}{key}\' was '
+                                     f'removed')
+                if key_type == 'added':
+                    diff_list.append(f'Property \'{"".join(path)}{key}\' '
+                                     f'was added with value: {value}')
+            if key == sorted_keys[-1] and path:
+                path.pop()
+    inner(diff_dict)
     return diff_list
 
 
