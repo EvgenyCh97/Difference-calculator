7e275a7899c00d04111d2a63561533b797b97c8b
Евгений Чайка <chay67ka26@gmail.com>
2023-06-05 11:43:15 +0800

    rename keys_concatenation to all_keys in get_diff func

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -25,9 +25,9 @@ def parse(content, extension):
 def get_diff(dict1, dict2):
     diff = dict()
     keys_set1, keys_set2 = set(dict1.keys()), set(dict2.keys())
-    keys_concatenation = keys_set1.union(keys_set2)
+    all_keys = keys_set1.union(keys_set2)
     added_keys = keys_set2.difference(keys_set1)
-    for key in keys_concatenation:
+    for key in all_keys:
         value1 = dict1.get(key)
         value2 = dict2.get(key)
         if type(value1) == dict and type(value2) == dict:
